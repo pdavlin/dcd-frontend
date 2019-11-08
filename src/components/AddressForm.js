e735959ca58cce9a29f6911859709e5926b5521d
Patrick Davlin <patrick@davlin.io>
2019-11-07 20:01:26 -0600

    implement marker set/remove functionality

--- src/components/AddressForm.js
+++ src/components/AddressForm.js
@@ -8,6 +8,7 @@ export const AddressForm = (props) => {
 
   const { setLatLngPair } = useAppContext();
   const [address, setAddress] = useState('1819 Farnam Street');
+  const [isSubmitted, setIsSubmitted] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -18,24 +19,45 @@ export const AddressForm = (props) => {
         setLatLngPair([response.data.results[0].geometry.location.lat, response.data.results[0].geometry.location.lng]);
       } else console.error("uh oh", response)
     })
+    setIsSubmitted(true);
   }
-  return (
-    <Box as='form' onSubmit={handleSubmit} width={1}>
-      <Box width={1}>
-        <Label htmlFor='address'>Enter the <b>&nbsp;first line&nbsp;</b> of your street address:</Label>
-        <Input
-          id='address'
-          name='address'
+
+  const newSearch = () => {
+    setIsSubmitted(false);
+  }
+
+  if (!isSubmitted) {
+    return (
+      <Box as='form' onSubmit={handleSubmit} width={1}>
+        <Box width={1}>
+          <Label htmlFor='address'>Enter the <b>&nbsp;first line&nbsp;</b> of your street address:</Label>
+          <Input
+            id='address'
+            name='address'
+            sx={{
+              ':focus': {
+                borderColor: 'primary',
+                boxShadow: '0 0 0 2px #55d6be'
+              }
+            }}
+            value={address}
+            onChange={({ target }) => { setAddress(target.value) }}
+          />
+        </Box>
+        <Button
+          variant='hoverable'
+          width={1 / 2}
+          mt={1}
           sx={{
-            ':focus': {
-              borderColor: 'primary',
-              boxShadow: '0 0 0 2px #55d6be'
+            ':hover': {
+              backgroundColor: 'primary',
+              color: 'background'
             }
-          }}
-          value={address}
-          onChange={({ target }) => { setAddress(target.value) }}
-        />
+          }} >Submit</Button>
       </Box>
+    )
+  } else {
+    return (
       <Button
         variant='hoverable'
         width={1 / 2}
@@ -45,7 +67,12 @@ export const AddressForm = (props) => {
             backgroundColor: 'primary',
             color: 'background'
           }
-        }} >Submit</Button>
-    </Box>
-  )
+        }}
+        onClick={
+          newSearch
+        }>
+        New Search
+      </Button>
+    )
+  }
 }
